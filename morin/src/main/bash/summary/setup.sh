49da2d3f91480acd428ae87987bb12c4ce266884
brice-morin <brice.morin1@gmail.com>
2018-07-10 10:45:35 +0200

    small fixes

--- morin/src/main/bash/summary/setup.sh
+++ morin/src/main/bash/summary/setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-N=100 # Number of diversified version to make
-LANGUAGES=(nodejs go arduino) # Languages to use
+N=10 # Number of diversified version to make
+#LANGUAGES=(nodejs go arduino) # Languages to use
+LANGUAGES=(nodejs go) # Languages to use
 
 # Please make sure you have the following list of environment variebles set up:
-# - THINGML_REGISTRY=/path/to/compilers.registry-2.0.0-SNAPSHOT-jar-with-dependencies.jar
-#     The ThingML compiler registry jar (or the ThingMLCli from dist)
 # - ARDUINO_DIR=/path/to/Arduino
 #     The Ardunio IDE installation folder
 # - ARDUINO_BUILDER_DIR=/path/to/arduino-builder
 #     Path to arduino-builder folder
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+THINGML_REGISTRY=$DIR/../../../../bin/thingml.jar
 BASEMODELDIR=$DIR/../../resources/experiments
 TARGETDIR=$DIR/../../../../target
 DIVERSIFIER=$TARGETDIR/thingml.diversifier-1.0.0-SNAPSHOT-jar-with-dependencies.jar
