019736f13aab951b6851392d3c3dddbd7541a56c
brice-morin <brice.morin1@gmail.com>
2018-07-10 10:17:57 +0200

    some small fixes here and there

--- morin/src/main/bash/bytes/setup.sh
+++ morin/src/main/bash/bytes/setup.sh
@@ -4,14 +4,13 @@ N=10 # Number of diversified version to make
 LANGUAGES=(nodejs) # Languages to use
 
 # Please make sure you have the following list of environment variebles set up:
-# - THINGML_REGISTRY=/path/to/compilers.registry-2.0.0-SNAPSHOT-jar-with-dependencies.jar
-#     The ThingML compiler registry jar (or the ThingMLCli from dist)
 # - ARDUINO_DIR=/path/to/Arduino
 #     The Ardunio IDE installation folder
 # - ARDUINO_BUILDER_DIR=/path/to/arduino-builder
 #     Path to arduino-builder folder
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+THINGML_REGISTRY=$DIR/../../../../bin/thingml.jar
 BASEMODELDIR=$DIR/../../resources/experiments
 TARGETDIR=$DIR/../../../../target
 DIVERSIFIER=$TARGETDIR/thingml.diversifier-1.0.0-SNAPSHOT-jar-with-dependencies.jar
